cf5f013c9bcbe95ce74b80843c076515a6f7ae46
lagerdigal1996 <kpogohhymes@hotmail.com>
2025-08-31 09:53:55 -0500

    feat: initial commit for src/App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Wireframe Prototyping Tool</h1>
+      <p>Welcome to the Wireframe Prototyping Tool!</p>
+    </div>
+  );
+};
+
+export default App;
